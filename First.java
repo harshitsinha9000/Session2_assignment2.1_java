a54bb6d3d71930b53bad15d40ee4a8f0b51bc906
harshitsinha9000 <harshitsinha9000@gmail.com>
2017-02-17 13:35:33 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,29 @@
+package java_session2;
+
+import java.util.Scanner;
+
+public class First {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+		String str;
+		System.out.println("Enter your name");
+		str=sc.next();
+		
+		if(System.out.printf(str, args)!=null)
+		{
+			
+		}
+		
+		//for(int i=0;i<1,System.out.println(str);i++)
+		//{
+			
+		//}
+		
+		
+		
+	}
+
+}
